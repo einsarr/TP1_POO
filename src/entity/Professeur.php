8078820f8e1e28271d29b72983247415147162be
Moussa SARR <einsarr@gmail.com>
2019-12-14 22:10:11 +0000

    Ajout de la classe Professeur

---
+++ src/entity/Professeur.php
@@ -0,0 +1,29 @@
+<?php
+require "Personne.php";
+class Professeur extends Personne
+{
+    private $matiere;
+    public function __construct($matiere,$nom,$prenom)
+    {
+        parent::__construct($nom,$prenom);
+        $this->matiere = $matiere;
+    }
+    public function getMatiere()
+    {
+        return $this->matiere;
+    }
+    public function setMatiere($matiere)
+    {
+        $this->matiere = $matiere;
+    }
+    public function affectation()
+    {
+        parent::saisie();
+        $this->matiere = "PHP";
+    }
+    public function affichage()
+    {
+        parent::affiche();
+        echo $this->matiere;
+    }
+}
