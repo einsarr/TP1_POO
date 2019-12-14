7e9ab4fea26ead52ac97eabe7f2f5cd5a96e125d
Moussa SARR <einsarr@gmail.com>
2019-12-14 22:07:26 +0000

    Ajout de la classe Etudiant

---
+++ src/entity/Etudiant.php
@@ -0,0 +1,29 @@
+<?php
+require "Personne.php";
+class Etudiant extends Personne
+{
+    private $matricule;
+    public function __construct($matricule,$nom,$prenom)
+    {
+        parent::__construct($nom,$prenom);
+        $this->matricule = $matricule;
+    }
+    public function getMatricule()
+    {
+        return $this->matricule;
+    }
+    public function setMatricule($matricule)
+    {
+        $this->matricule = $matricule;
+    }
+    public function affectation()
+    {
+        parent::saisie();
+        $this->matricule = "FHR6783Y";
+    }
+    public function affichage()
+    {
+        parent::affiche();
+        echo $this->matricule;
+    }
+}
