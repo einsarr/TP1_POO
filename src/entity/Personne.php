13259f7b5a0ee5493e58ca280283d25a34bf93df
Moussa SARR <einsarr@gmail.com>
2019-12-14 21:53:17 +0000

    First commit and add Personne.php class

---
+++ src/entity/Personne.php
@@ -0,0 +1,37 @@
+<?php
+class Personne{
+    protected $nom;
+    protected $prenom;
+
+    public function __construct($nom,$prenom)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+    public function saisie()
+    {
+        $this->nom = "SARR";
+        $this->prenom = "Moussa";
+    }
+    public function affiche()
+    {
+        echo "Prenom : ".$this->prenom." Nom : ".$this->nom;
+    }
+
+}
